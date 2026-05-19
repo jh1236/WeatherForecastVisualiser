e274d44fcb32dd1674b2fff48f6e6ca926c5a543
Jared Healy <healy_jared@yahoo.com>
2026-05-19 16:42:20 +0800

    Stop pre-fetching when not in prod

--- instrumentation.node.ts
+++ instrumentation.node.ts
@@ -22,6 +22,10 @@ async function getDataForComingTimeframe() {
 }
 
 export async function register() {
+    if (process.env.NODE_ENV === 'development') {
+        //
+        return
+    }
     await getDataForComingTimeframe();
 
     CronJob.from({
